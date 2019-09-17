8b866467d8ab721ac4552b23f4ab53eea5553b84
leszekmajd <leszek@hiveberg.pl>
2019-09-17 17:38:08 +0200

    Initial commit

---
+++ src/main/kotlin/pl/leszekmajda/hospital/HospitalApplication.kt
@@ -0,0 +1,11 @@
+package pl.leszekmajda.hospital
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HospitalApplication
+
+fun main(args: Array<String>) {
+    runApplication<HospitalApplication>(*args)
+}
