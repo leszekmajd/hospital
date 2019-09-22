80d8ddf8a6a503f80bb8fde5d53d3ab5905e1b4d
leszekmajd <leszek@hiveberg.pl>
2019-09-22 14:13:08 +0200

    class fix

--- src/main/kotlin/pl/leszekmajda/hospital/dao/Patient.kt
+++ src/main/kotlin/pl/leszekmajda/hospital/dao/Patient.kt
@@ -3,7 +3,6 @@ package pl.leszekmajda.hospital.dao
 import javax.persistence.*
 
 @Entity
-class Patient(var name: String, var adres: String, var @Id @GeneratedValue, @Id @GeneratedValue  var id: Long? = null) {
-
+class Patient (var name: String="", var adres: String="",  @Id @GeneratedValue  var id: Long? = null){
 
 }
