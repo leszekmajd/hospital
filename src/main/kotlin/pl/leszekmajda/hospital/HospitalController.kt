4fb94c179213d761bb524476e9f71bf9da04ee27
leszekmajd <leszek@hiveberg.pl>
2019-09-25 16:50:30 +0200

    Create and delete functionality of every model added

--- src/main/kotlin/pl/leszekmajda/hospital/HospitalController.kt
+++ src/main/kotlin/pl/leszekmajda/hospital/HospitalController.kt
@@ -3,8 +3,11 @@ package pl.leszekmajda.hospital
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.util.UriComponentsBuilder
+import pl.leszekmajda.hospital.dao.*
+import java.time.LocalDateTime
+import javax.servlet.http.HttpServletRequest
 
 @Controller
 class HospitalController(private val appointmentRepository: AppointmentRepository,
@@ -24,13 +27,37 @@ class HospitalController(private val appointmentRepository: AppointmentRepositor
         return "patients"
     }
 
+    @PostMapping("/patient")
+    fun addPatient(request: HttpServletRequest, model: Model): String {
+        patientRepository.save(Patient(request.getParameter("name"), request.getParameter("surname"),
+                request.getParameter("address")))
+        model["title"] = "RSQ Hospital > List of patients"
+        model["patients"] = patientRepository.findAll().map { it }
+        return "patients"
+    }
+
     @GetMapping("/patient/{id}")
     fun patient(@PathVariable id: Long, model: Model): String {
         val patient = patientRepository.findById(id).get()
-        println(patient)
         model["title"] = "RSQ Hospital > Patient id: ${patient.id}"
         model["patient"] = patient
         return "patient"
+    }
+
+    @GetMapping("/patient/delete/{id}")
+    fun deletePatient(@PathVariable id: Long, model: Model): String {
+        patientRepository.deleteById(id)
+        model["title"] = "RSQ Hospital > List of doctors :: Patient id: $id deleted successfuly"
+        model["patients"] = patientRepository.findAll().map { it }
+        return "patients"
+    }
+
+    @GetMapping("/doctor/delete/{id}")
+    fun deleteDoctor(@PathVariable id: Long, model: Model): String {
+        doctorRepository.deleteById(id)
+        model["title"] = "RSQ Hospital > List of doctors :: Doctor id: $id deleted successfuly"
+        model["doctors"] = doctorRepository.findAll().map { it }
+        return "doctors"
     }
 
     @GetMapping("/doctor")
@@ -38,6 +65,15 @@ class HospitalController(private val appointmentRepository: AppointmentRepositor
         model["title"] = "RSQ Hospital > List of doctors"
         model["doctors"] = doctorRepository.findAll().map { it }
         return "doctors"
+    }
+
+    @PostMapping("/doctor")
+    fun addDoctor(request: HttpServletRequest, model: Model): String {
+        doctorRepository.save(Doctor(request.getParameter("name"), request.getParameter("surname"),
+                request.getParameter("specialisation")))
+        model["title"] = "RSQ Hospital > List of doctors"
+        model["doctors"] = doctorRepository.findAll().map { it }
+        return "doctor"
     }
 
     @GetMapping("/doctor/{id}")
@@ -51,6 +87,34 @@ class HospitalController(private val appointmentRepository: AppointmentRepositor
     @GetMapping("/appointment")
     fun appointments(model: Model): String {
         model["title"] = "RSQ Hospital > List of appointments"
+        model["appointments"] = appointmentRepository.findAll().map { it }
+        model["patients"]  = patientRepository.findAll().map { it }
+        model["doctors"] = doctorRepository.findAll().map { it }
+        return "appointments"
+    }
+
+    @PostMapping("/appointment")
+    fun addAppointment(request: HttpServletRequest, model: Model): String {
+        val patientId=request.getParameter("patient-id").toLong()
+        val doctorId=request.getParameter("doctor-id").toLong()
+        val date= LocalDateTime.parse(request.getParameter("date"))
+        val place=request.getParameter("place")
+        val patient=patientRepository.findById(patientId).get()
+        val doctor=doctorRepository.findById(doctorId).get()
+        appointmentRepository.save(Appointment(date,place,patient,doctor))
+        model["title"] = "RSQ Hospital > List of Appointments"
+        model["patients"]  = patientRepository.findAll().map { it }
+        model["doctors"] = doctorRepository.findAll().map { it }
+        model["appointments"] = appointmentRepository.findAll().map { it }
+        return "appointments"
+    }
+
+    @GetMapping("/appointment/delete/{id}")
+    fun deleteAppointment(@PathVariable id: Long, model: Model): String {
+        appointmentRepository.deleteById(id)
+        model["title"] = "RSQ Hospital > List of doctors :: Appointment id: $id deleted successfuly"
+        model["patients"]  = patientRepository.findAll().map { it }
+        model["doctors"] = doctorRepository.findAll().map { it }
         model["appointments"] = appointmentRepository.findAll().map { it }
         return "appointments"
     }
